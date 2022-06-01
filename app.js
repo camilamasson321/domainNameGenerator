331ff7a881c90bd052ec4e0861a521244918ce8f
camilamasson321 <camilamasson321@gmail.com>
2022-06-01 22:15:42 +0000

    draft

---
+++ app.js
@@ -0,0 +1,23 @@
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+//   window.onload = function() {
+    //write your code here
+    let pronoun = ["these", "those", "that", "ours"];
+    let adjective = ["amused", "cheerful", "calm", "disturbed"];
+    let noun = ["people", "doors", "friends", "guys"];
+
+    // const domainName = "OMG! You will not believe me but...";
+    // document.head.innerHTML =
+
+      pronoun [getRandomInt(4)] +
+      " " +
+      adjective [getRandomInt(4)] +
+      " " +
+      noun [getRandomInt(4)] +
+      " " +
+      ".com";
+  };
+  console.log()
